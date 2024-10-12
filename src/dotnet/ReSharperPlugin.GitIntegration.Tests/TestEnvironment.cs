0c6b1dfdf3301baf440c37bf826d40aab410bb63
Duta-Sebastian <sebiduta2004@gmail.com>
2024-10-12 20:41:19 +0300

    First Commit - Template

---
+++ src/dotnet/ReSharperPlugin.GitIntegration.Tests/TestEnvironment.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using NUnit.Framework;
+
+[assembly: Apartment(ApartmentState.STA)]
+
+namespace ReSharperPlugin.GitIntegration.Tests
+{
+    [ZoneDefinition]
+    public class GitIntegrationTestEnvironmentZone : ITestsEnvZone, IRequire<PsiFeatureTestZone>, IRequire<IGitIntegrationZone> { }
+
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>, IRequire<GitIntegrationTestEnvironmentZone> { }
+
+    [SetUpFixture]
+    public class GitIntegrationTestsAssembly : ExtensionTestEnvironmentAssembly<GitIntegrationTestEnvironmentZone> { }
+}
